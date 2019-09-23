7c21cefccaf4c8657e135b3e22cf08fda965c8c9
Jean Michel <jeanmichel@sabrys-mbp-2.lan>
2019-09-22 21:35:22 -0700

    two_arr_median.cpp

--- two_arr_median.cpp
+++ two_arr_median.cpp
@@ -2,59 +2,62 @@
 using namespace std;
 using namespace std::chrono;
 
-int mini(int a, int b) 
+/* Needs more work...
+ */
+
+int mini(int a, int b)
 {
-  return (a < b) ? a : b;
+  return a < b ? a : b;
 }
 
 int maxi(int x, int y)
 {
-  return (x > y) ? x : y;
+  return x > y ? x : y;
 }
 
 double sortedArrayMedian(vector<int>& a, vector<int>& b)
 {
-  int min = 0, m, i, j;
-  int max = a.size();
-  int max_2 = b.size();
-  int total = max + max_2;
+  int aSize = a.size();
+  int bSize = b.size();
 
-  while (min <= max)
+  if (aSize > bSize) return sortedArrayMedian(b, a);
+
+  int min_i = 0, max_i = aSize, i, j;
+  int h = (aSize + bSize + 1) / 2;
+  int median = 0;
+
+  while (min_i <= max_i)
     {
-      i = (min + max)/2;
-      j = ((max + max_2 + 1) / 2) - i;
+      i = (min_i + max_i) / 2;
+      j = h - i;
 
-      if (i < max && j > 0 && b[j - 1] > a[i]) min = i + 1;
-
-      else if (i > 0 && j > max_2 && b[j] < a[i - 1]) max = i - 1;
-
-      else
-	{
-	  if (i == 0) m = b[j - 1];
-	  else if (j == 0) m = a[i - 1];
-	  else m = maxi(a[i - 1], b[j - 1]);
-	  break;
-		       
-	}
+      if (i < aSize && j > 0 && b[j - 1] > a[i]) min_i = i + 1;
+      else if (i > 0 && j < bSize && b[j] < a[i - 1]) max_i = i - 1;
+      else {
+	if (i == 0) median = b[j - 1];
+	else if (j == 0) median = a[i - 1];
+	else median = maxi(a[i - 1], b[j - 1]);
+	break;
+      }
     }
-  if (total % 2 > 0) return (double)m;
-  if (i == max) return (m + b[j]) / 2.0;
-  if (j == max_2) return (m + a[i]) / 2.0;
 
-  return (m + mini(a[i], b[j])) / 2.0;
+  if ((aSize + bSize) % 2 != 0) return median;
+  if (i == aSize) return (median + b[j]) / 2.0;
+  if (j == bSize) return (median + a[i]) / 2.0;
+
+  return (median + mini(a[i], b[j])) / 2.0;
 }
 
 int main()
 {
-  vector<int> arr1 = {3,5,10,11,17};
-  vector<int> arr2 = {9,13,20,21,23,27};
+  vector<int> arr1 = {4,5};
+  vector<int> arr2 = {1,2,3};
 
   auto start = high_resolution_clock::now();
   cout << sortedArrayMedian(arr1, arr2) << endl;
   auto stop = high_resolution_clock::now();
-
   auto duration = duration_cast<microseconds>(stop - start);
-
   cout << duration.count() << " microseconds" <<endl;
+  
   return 0;
 }
